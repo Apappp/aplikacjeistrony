db0f0baa3049d73fe29f6f311199f25fc3b1444d
Apappp <90259436+Apappp@users.noreply.github.com>
2022-05-20 07:27:24 +0200

    Add files via upload

---
+++ obiektowe1/Samochod.js
@@ -0,0 +1,13 @@
+class Samochod {
+    constructor(marka, model, cena, rocznik, spalanie){
+        this.marka = marka;
+        this.model = model;
+        this.cena = cena;
+        this.rocznik = rocznik;
+        this.spalanie = spalanie;
+    }
+    wiek(){
+        return 2022-this.rocznik;
+    }
+    koszt(){}
+}
